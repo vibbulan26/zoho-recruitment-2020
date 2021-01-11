656204fa8d0448b7a660864cb670ae1540ced0f3
vibbulan26 <77238512+vibbulan26@users.noreply.github.com>
2021-01-11 09:14:47 +0530

    Zoho Software Developer Role Program
    Zoho Software Developer Role Program

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j;
+    cout<<"Enter Row Limit:";
+    cin>>n;
+    for(i = n; i >= 1; i--) {
+        for (j = n; j >= 1; j--) {
+            if (i >= j)
+            cout<<i;
+            else
+            cout<<j;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
